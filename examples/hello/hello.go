b9aaf6eb7f7d586d1ed501b583b669cb8fe819c7
Vikas Gorur <vikasgp@gmail.com>
2015-06-04 10:38:07 -0700

    Add hello example

---
+++ examples/hello/hello.go
@@ -0,0 +1,25 @@
+/*
+
+Command hello demonstrates how to use the bespoke package. When not customized
+it prints an error message.
+
+If it's turned into a bespoke binary with a map of the form
+	{"name": "vikas"}
+
+it prints "hello vikas".
+*/
+package main
+
+import (
+	"fmt"
+	"github.com/vikasgorur/bespoke"
+)
+
+func main() {
+	m, err := bespoke.Map()
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("hello " + m["name"])
+	}
+}
